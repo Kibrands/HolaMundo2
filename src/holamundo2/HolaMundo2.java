6ce76fdc20389957b8fb2d0cf801f5d758b4b2b9
Kibrands <jjextreme13@gmail.com>
2019-04-01 12:13:26 +0200

    Main class modified

---
+++ src/holamundo2/HolaMundo2.java
@@ -0,0 +1,14 @@
+package holamundo2;
+
+/**
+ *
+ * @author Kibrands
+ */
+public class HolaMundo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde el programa 2");
+	System.out.println("Hola Mundo desde el programa 2");
+    }
+
+}
