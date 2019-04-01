7e781ab41fbe29ac479a34f8cbbe94e580beb6bc
Kibrands <jjextreme13@gmail.com>
2019-04-01 12:34:02 +0200

    Another main class modification

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -9,7 +9,7 @@ public class HolaMundo2 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde el programa 2");
 	System.out.println("Abril 2019");
-	System.out.println("IES Luis Vélez de Guevara");
+	System.out.println("Écija, IES Luis Vélez de Guevara");
     }
 
 }
