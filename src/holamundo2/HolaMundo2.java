c6f0fb1661fba373b187c0099245ad1946667979
Kibrands <jjextreme13@gmail.com>
2019-04-01 12:15:44 +0200

    Main class modified again

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -8,7 +8,8 @@ public class HolaMundo2 {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde el programa 2");
-	System.out.println("Hola Mundo desde el programa 2");
+	System.out.println("Abril 2019");
+	System.out.println("IES Luis Vélez de Guevara");
     }
 
 }
